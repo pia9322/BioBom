23dbfa9dbd360c32843bd307480b85ff10ab377a
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-12-09 11:40:27 +0900

    header
    sub_depth_event

--- js/common.js
+++ js/common.js
@@ -189,23 +189,15 @@ $(document).ready(function(){
 
     // jj_add window Scroll event
     $(window).scroll(function () {
-        let y = $(this).scrollTop(),
+    let y = $(this).scrollTop(),
         scrolltop = $(window).scrollTop();
-        // IndexHome = '/',
-        // path = location.pathname;
-
-        function IndexPop() {
-            let popbanner = document.querySelector('.event_banner_01'),
-                popbannerHei = popbanner.offsetTop;
-
-            if (y >= popbannerHei) {
-                $('.banner_pop').addClass("active");
-            } else {
-                $('.banner_pop').removeClass("active");
-            }
-        }
+        IndexHome = '/',
+        path = location.pathname,
+        sub_depth = $('.header_center .depth_01');
 
         (y >= 70) ? $("#header").addClass('active') : $("#header").removeClass('active');
+        (path.indexOf("/about/") >= 0) ? sub_depth[2].style['display'] = 'flex' : sub_depth.style['display'] = 'none'
+
     });
     // jj_add window Scroll event
 
@@ -213,6 +205,16 @@ $(document).ready(function(){
 
 
 
+    // function IndexPop() {
+    //     let popbanner = document.querySelector('.event_banner_01'),
+    //         popbannerHei = popbanner.offsetTop;
+
+    //     if (y >= popbannerHei) {
+    //         $('.banner_pop').addClass("active");
+    //     } else {
+    //         $('.banner_pop').removeClass("active");
+    //     }
+    // }
 
 
 
